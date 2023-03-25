4ae59e4e6eba23c6db5970720095d567909d2954
AhmedAshrafM <ahmedashraff439@gmail.com>
2023-03-25 05:35:58 +0200

    Jwt token sent but doesn't return profile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Bind, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Dependencies, Bind, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 
+@Dependencies(AuthService)
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    this.authService = authService;
+  }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   @Bind(Request())
   async login(req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @Bind(Request())
+  getProfile(req) {
+    return req.user;
   }
 }
