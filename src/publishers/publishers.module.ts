358fcaa963dfb2f49bd52bfa77d0ab04eed726a5
AhmedAshrafM <ahmedashraff439@gmail.com>
2023-03-28 19:12:20 +0200

    publisher configured + module edited

--- src/publishers/publishers.module.ts
+++ src/publishers/publishers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PublishersService } from './services/publishers.service';
 import { PublishersController } from './controllers/publishers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Publisher } from 'src/typeorm/entities/Publisher';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Publisher])],
   providers: [PublishersService],
   controllers: [PublishersController]
 })
