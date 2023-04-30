419c521c2313867e848cd66cb082e5ea08dba730
AhmedAshrafM <ahmedashraff439@gmail.com>
2023-04-30 09:06:42 +0300

    Reservations crud optimized

--- src/reservations/services/reservations.service.ts
+++ src/reservations/services/reservations.service.ts
@@ -14,41 +14,66 @@ import { dtoToEntity } from '../mapper/reservation.mapper';
 
 @Injectable()
 export class ReservationsService {
-    constructor(
-        @InjectRepository(BookStock) private bookStockRepository: Repository<BookStock>,
-        @InjectRepository(User) private userRepository: Repository<User>,
-        @InjectRepository(Reservation) private reservationRepository: Repository<Reservation>,
-        @InjectRepository(Audit) private loggerRepo: Repository<Audit>,
-        @InjectRepository(Notification) private notRepo: Repository<Notification>
-    ){}
+  constructor(
+    @InjectRepository(BookStock)
+    private bookStockRepository: Repository<BookStock>,
+    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Reservation)
+    private reservationRepository: Repository<Reservation>,
+    @InjectRepository(Audit) private loggerRepo: Repository<Audit>,
+    @InjectRepository(Notification) private notRepo: Repository<Notification>,
+  ) {}
 
-    fetchReservations(){
-        return this.reservationRepository.find();
-    }
-    async createReservation(reservationDetails: createReservationDTO){
-        const user = await this.userRepository.findOneById(reservationDetails.userId);
-        const bookStock = await this.bookStockRepository.findOneById(reservationDetails.bookStockId)
-        let newReservation : Reservation = dtoToEntity(reservationDetails);
-        newReservation.addBookStock(bookStock);
-        newReservation.addUser(user);
-        let newNotifcation : Notification = eventToNotification(newReservation,newReservation.id,"New Reservation")
-        let newLog: Audit = entityToLog("New Reservation",newReservation,"Reservations")
-        this.loggerRepo.save(newLog)
-        this.notRepo.save(newNotifcation)
-        console.log(newReservation);
-        
-        return this.reservationRepository.save(newReservation);
-    }
-    async updateReservation(id: number, updateReservationDetails: updateReservationDTO) {
-        const user = await this.userRepository.findOneById(updateReservationDetails.userId);
-        const bookStock = await this.bookStockRepository.findOneById(updateReservationDetails.bookStockId)
-        let updatedReservation : Reservation = dtoToEntity(updateReservationDetails);
-        updatedReservation.addBookStock(bookStock);
-        updatedReservation.addUser(user);
+  fetchReservations() {
+    return this.reservationRepository.find({
+      relations: ['userId', 'bookStockId'],
+    });
+  }
+  async createReservation(reservationDetails: createReservationDTO) {
+    const user = await this.userRepository.findOneById(
+      reservationDetails.userId,
+    );
+    const bookStock = await this.bookStockRepository.findOneById(
+      reservationDetails.bookStockId,
+    );
+    let newReservation: Reservation = dtoToEntity(reservationDetails);
+    newReservation.addBookStock(bookStock);
+    newReservation.addUser(user);
+    let newNotifcation: Notification = eventToNotification(
+      newReservation,
+      newReservation.id,
+      'New Reservation',
+    );
+    let newLog: Audit = entityToLog(
+      'New Reservation',
+      newReservation,
+      'Reservations',
+    );
+    this.loggerRepo.save(newLog);
+    this.notRepo.save(newNotifcation);
+    console.log(newReservation);
 
-        return await this.reservationRepository.update( id , { ...updatedReservation });
-      }
-      async deleteReservation(id: number) {
-        return this.reservationRepository.delete(id);
-      }
+    return this.reservationRepository.save(newReservation);
+  }
+  async updateReservation(
+    id: number,
+    updateReservationDetails: updateReservationDTO,
+  ) {
+    const user = await this.userRepository.findOneById(
+      updateReservationDetails.userId,
+    );
+    const bookStock = await this.bookStockRepository.findOneById(
+      updateReservationDetails.bookStockId,
+    );
+    let updatedReservation: Reservation = dtoToEntity(updateReservationDetails);
+    updatedReservation.addBookStock(bookStock);
+    updatedReservation.addUser(user);
+
+    return await this.reservationRepository.update(id, {
+      ...updatedReservation,
+    });
+  }
+  async deleteReservation(id: number) {
+    return this.reservationRepository.delete(id);
+  }
 }
